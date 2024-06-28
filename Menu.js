a6d0fc6d3aa6e894ac397adc922a2d45841fa317
unknown <laxitanaik001@gmail.com>
2024-06-29 00:02:53 +0530

    initial commit

---
+++ Menu.js
@@ -0,0 +1,12 @@
+
+
+    function menu(){
+        
+        var child = document.querySelector('.mobile_navigation');
+        
+             child.classList.toggle("show");
+             
+
+
+        
+    }
